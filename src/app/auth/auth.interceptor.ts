47ab85a86123dfeda43fc750eaf43d1a463a0503
Rafael Carvalho <rafael.faria.carvalho@gmail.com>
2019-11-04 22:21:27 -0300

    token

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,10 +1,17 @@
-import { HttpInterceptor, HttpRequest, HttpHandler } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { catchError } from 'rxjs/operators';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() {
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
 
   }
 
@@ -14,7 +21,19 @@ export class AuthInterceptor implements HttpInterceptor {
       const authReq = req.clone(
         {setHeaders: {Authorization: token}}
       );
-      return next.handle(authReq);
+      return next.handle(authReq)
+        .pipe(
+          catchError((error)=>{
+            console.log(error);
+            if(error instanceof HttpErrorResponse) {
+              if (error.status === 401) {
+                this.authService.logout();
+                this.router.navigateByUrl('/auth/login');
+              }
+            }
+            return throwError(error);
+          })
+        );
     }
     return next.handle(req);
   }
